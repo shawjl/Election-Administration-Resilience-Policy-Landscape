611855b09c3d75a97059abb48e7b6ca40383ccbb
Jake Shaw <jake.shaw@umsl.edu>
2024-07-25 14:05:53 -0500

    Updated with other helpful links

--- Getting Started With Git and GitHub.R
+++ Getting Started With Git and GitHub.R
@@ -3,3 +3,8 @@
 # Use this website to familiarize yourself with how to connect GitHub to R and
 # even create a repository of your own: 
 # https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
+
+# Other helpful links
+
+# How to create folders:
+# https://stackoverflow.com/questions/12258399/how-do-i-create-a-folder-in-a-github-repository
