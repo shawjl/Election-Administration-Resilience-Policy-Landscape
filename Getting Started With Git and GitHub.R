17bdd850370c671c7344690605db5ea83a379d6c
Jake Shaw <jake.shaw@umsl.edu>
2024-07-25 10:24:24 -0500

    Added instructions to Getting Started With Git and GitHub

---
+++ Getting Started With Git and GitHub.R
@@ -0,0 +1,5 @@
+## Getting Started With Git and GitHub
+
+# Use this website to familiarize yourself with how to connect GitHub to R and
+# even create a repository of your own: 
+# https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
